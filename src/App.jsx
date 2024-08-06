6dfe84f4295e65221d7eeee77d3ce603022b6f81
BillStephens2022 <two4onebill@yahoo.com>
2024-08-06 07:39:54 -0400

    added styled components package, added Button & input components, set up GlobalStyles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
+import styled from "styled-components";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import GlobalStyles from "./styles/GlobalStyles";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
   return (
-    <div>
-      <h1>Hello, React!</h1>
-    </div>
-  )
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => alert("Check In!!")}>Check In</Button>
+        <Button onClick={() => alert("Check Out!!")}>Check Out</Button>
+        <Input type="number" placeholder="Number of guests" />
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
