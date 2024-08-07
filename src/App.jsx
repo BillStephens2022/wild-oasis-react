3fd79e63e7ea7b41bcf95d491fcb44e5146d2942
BillStephens2022 <two4onebill@yahoo.com>
2024-08-07 08:10:35 -0400

    install and set up react-query provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import GlobalStyles from "./styles/GlobalStyles";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Dashboard from "./pages/Dashboard";
 import Bookings from "./pages/Bookings";
 import Cabins from "./pages/Cabins";
@@ -9,10 +10,20 @@ import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
+import GlobalStyles from "./styles/GlobalStyles";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60,
+    },
+  },
+});
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -29,7 +40,7 @@ function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
 
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -9,11 +9,12 @@ function Cabins() {
       console.log(cabins);
     });
   }, []);
-  
+
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+      <img src="https://wixzhqakwldlbxwzslkw.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg" alt="cabin 1" />
     </Row>
   );
 }
