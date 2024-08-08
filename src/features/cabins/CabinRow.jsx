fdbfb89d62e49f5e38538026268b280e19e68d2c
BillStephens2022 <two4onebill@yahoo.com>
2024-08-08 07:05:12 -0400

    added functionality to delete a cabin

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteCabin } from "../../services/apiCabins";
 
 const TableRow = styled.div`
   display: grid;
@@ -41,18 +43,39 @@ const Discount = styled.div`
 `;
 
 function CabinRow({ cabin }) {
+  const {
+    id: cabinId,
+    name,
+    maxCapacity,
+    regularPrice,
+    discount,
+    image,
+  } = cabin;
 
-  const { name, maxCapacity, regularPrice, discount, image } = cabin;
+  const queryClient = useQueryClient();
+
+  const { isLoading: isDeleting, mutate } = useMutation({
+    mutationFn: deleteCabin,
+    onSuccess: () => {
+      alert("Cabin deleted successfully");
+      queryClient.invalidateQueries({
+        queryKey: "cabins",
+      }); // invalidates the cache, basically causing a refetch of data since data is now stale after deleting a cabin
+    },
+    onError: (err) => alert(err.message),
+  });
+
   return (
-
     <TableRow role="row">
       <Img src={image} alt="cabin image" />
       <Cabin>{name}</Cabin>
       <div>Fits up to {maxCapacity}</div>
       <Price>{formatCurrency(regularPrice)}</Price>
       <Discount>{formatCurrency(discount)}</Discount>
-      <button>Delete</button>
-      </TableRow>
+      <button onClick={() => mutate(cabinId)} disabled={isDeleting}>
+        Delete
+      </button>
+    </TableRow>
   );
 }
 
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -8,3 +8,13 @@ export async function getCabins() {
   }
   return cabins;
 }
+
+export async function deleteCabin(id) {
+  const { error } = await supabase.from("cabins").delete().eq("id", id);
+  if (error) {
+    console.error(error);
+    throw new Error("Cabin could not be deleted");
+  }
+}
+
+
