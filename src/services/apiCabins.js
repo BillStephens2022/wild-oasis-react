c7230f20fa5b56964c9659ba8d4cc3f35cbdad8e
BillStephens2022 <two4onebill@yahoo.com>
2024-08-08 08:01:45 -0400

    added functionality to add a new cabin to the database

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,12 +1,12 @@
 import supabase from "./supabase";
 
 export async function getCabins() {
-  const { data: cabins, error } = await supabase.from("cabins").select("*");
+  const { data, error } = await supabase.from("cabins").select("*");
   if (error) {
     console.error(error);
     throw new Error("Cabins could not be loaded");
   }
-  return cabins;
+  return data;
 }
 
 export async function deleteCabin(id) {
@@ -17,4 +17,16 @@ export async function deleteCabin(id) {
   }
 }
 
+export async function createCabin(newCabin) {
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([newCabin])
+    .select();
 
+  if (error) {
+    console.error(error);
+    throw new Error("Cabin could not be created");
+  }
+
+  return data;
+}
