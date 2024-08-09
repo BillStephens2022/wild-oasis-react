ef95a1c131bdf0a034001cc4207572ca7ba4118f
BillStephens2022 <two4onebill@yahoo.com>
2024-08-09 09:12:40 -0400

    renamed useEditCabin hook to useUpdateCabin, added functionality to update settings

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -6,7 +6,7 @@ import Button from "../../ui/Button";
 import FileInput from "../../ui/FileInput";
 import Textarea from "../../ui/Textarea";
 import { useCreateCabin } from "./useCreateCabin";
-import { useEditCabin } from "./useEditCabin";
+import { useUpdateCabin } from "./useUpdateCabin";
 
 function CreateCabinForm({ cabinToEdit = {} }) {
   const { id: editId, ...editValues } = cabinToEdit;
@@ -19,14 +19,14 @@ function CreateCabinForm({ cabinToEdit = {} }) {
 
   const { isCreating, createCabin } = useCreateCabin();
 
-  const { isEditing, editCabin } = useEditCabin();
+  const { isUpdating, updateCabin } = useUpdateCabin();
 
-  const isWorking = isCreating || isEditing;
+  const isWorking = isCreating || isUpdating;
 
   function handleSubmitCabinForm(data) {
     const image = typeof data.image === "string" ? data.image : data.image[0];
     if (isEditSession)
-      editCabin(
+      updateCabin(
         { newCabinData: { ...data, image }, id: editId },
         {
           onSuccess: (data) => {
--- src/features/cabins/useUpdateCabin.js
+++ src/features/cabins/useUpdateCabin.js
@@ -2,18 +2,18 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { createEditCabin as createEditCabin } from "../../services/apiCabins";
 
-export function useEditCabin() {
+export function useUpdateCabin() {
   const queryClient = useQueryClient();
 
-  const { mutate: editCabin, isLoading: isEditing } = useMutation({
+  const { mutate: updateCabin, isLoading: isEditing } = useMutation({
     mutationFn: ({ newCabinData, id }) => createEditCabin(newCabinData, id),
     onSuccess: () => {
-      toast.success("Cabin successfully edited");
+      toast.success("Cabin successfully updated");
       queryClient.invalidateQueries({
         queryKey: "cabins",
-      }); // invalidates the cache, basically causing a refetch of data since data is now stale after adding a new cabin
+      }); // invalidates the cache, basically causing a refetch of data since data is now stale after editing a abin
     },
     onError: (err) => toast.error(err.message),
   });
-  return { isEditing, editCabin };
+  return { isEditing, updateCabin };
 }
