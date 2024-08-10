572687dcdcf575c98c21cd3536f5ba78aed827af
BillStephens2022 <two4onebill@yahoo.com>
2024-08-10 13:12:44 -0400

    added uploader for sample data

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import Logo from "./Logo";
 import MainNav from "./MainNav";
+import Uploader from "../data/Uploader";
+<data value=""></data>
 
 const StyledSidebar = styled.aside`
   background-color: var(--color-grey-0);
@@ -17,6 +19,7 @@ function Sidebar() {
     <StyledSidebar>
       <Logo />
       <MainNav />
+      <Uploader />
     </StyledSidebar>
   );
 }
