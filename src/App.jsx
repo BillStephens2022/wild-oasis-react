b87f53306b3adf32f628290a05fd3509b0a71ea9
BillStephens2022 <two4onebill@yahoo.com>
2024-08-12 13:33:25 -0400

    set up user sign up form

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Toaster } from "react-hot-toast";
 import Dashboard from "./pages/Dashboard";
 import Bookings from "./pages/Bookings";
 import Cabins from "./pages/Cabins";
-import Users from "./pages/Users";
+import NewUsers from "./pages/Users";
 import Settings from "./pages/Settings";
 import Account from "./pages/Account";
 import Login from "./pages/Login";
@@ -44,7 +44,7 @@ function App() {
             <Route path="bookings/:bookingId" element={<Booking />} />
             <Route path="checkin/:bookingId" element={<Checkin />} />
             <Route path="cabins" element={<Cabins />} />
-            <Route path="users" element={<Users />} />
+            <Route path="users" element={<NewUsers />} />
             <Route path="settings" element={<Settings />} />
             <Route path="account" element={<Account />} />
           </Route>
